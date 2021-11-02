320bc4f625d6e93d898ec6016f390b5f277df39d
Cristian Schneider <cristian.jhh12@gmail.com>
2021-11-02 08:42:05 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log ('Inicio')
+
+const ESPACIO = " ";
+let nombre = prompt ("ingrese su nombre");
+let apellido = prompt ("ingrese su apellido");
+let nombreApellido = nombre + ESPACIO + apellido;
+alert('Bienvenido a mi simulador,' + ESPACIO + nombreApellido);
+
+
+console.log ('Fin')
